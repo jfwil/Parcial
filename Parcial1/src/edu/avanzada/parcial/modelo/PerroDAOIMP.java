29f592c7e0dd204f90d142a49c68fd3f85649929
Juan <jfwilchesg@gmail.com>
2024-10-02 17:37:22 -0500

    Metodos modificar eliminar consultartodos, consultar, insertar

--- Parcial1/src/edu/avanzada/parcial/modelo/PerroDAOIMP.java
+++ Parcial1/src/edu/avanzada/parcial/modelo/PerroDAOIMP.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-
+import java.util.List;
 
 public class PerroDAOIMP implements PerroDAO {
 
@@ -18,13 +18,21 @@ public class PerroDAOIMP implements PerroDAO {
         st = null;
         rs = null;
     }
-    
+
     public void insertarPerro(Perro perro) throws SQLException {
-        
-        String insertar = "INSERT INTO Perros (raza, pais, grupoFCI, seccionFCI, apariencia, pelo, color, espalda, lomo, cola, pecho)";
-        
+
+        try {
+            con = Conexion.getConexion();
+            st = con.createStatement();
+            String insertar = "INSERT INTO Perros (raza, pais, grupoFCI, seccionFCI, apariencia, pelo, color, espalda, lomo, cola, pecho)";
+            st.executeUpdate(insertar);
+            st.close();
+            Conexion.desconectar();
+        } catch (SQLException ex) {
+            System.out.print("No se pudo realizar la insercion");
+        }
     }
-    
+
     @Override
     public Perro consultarPerro(String codigo) {
         Perro perro = null;
@@ -53,9 +61,10 @@ public class PerroDAOIMP implements PerroDAO {
         }
         return perro;
     }
+
     public ArrayList<Perro> listaDePerros() {
         ArrayList<Perro> misPerros = new ArrayList<Perro>();
-        String consulta = "SELECT * FROM Estudiantes";
+        String consulta = "SELECT * FROM PERROS";
         try {
             con = Conexion.getConexion();
             st = con.createStatement();
@@ -81,4 +90,71 @@ public class PerroDAOIMP implements PerroDAO {
         }
         return misPerros;
     }
+
+    public List<Perro> consultarTodosPerros(){
+        List<Perro> listaPerros = new ArrayList<>();
+        String consulta = "SELECT * FROM Perros";
+
+        try {
+            con = Conexion.getConexion();
+            st = con.createStatement();
+            rs = st.executeQuery(consulta);
+
+            while (rs.next()) {
+                Perro perro = new Perro();
+                perro.setRaza(rs.getString("Raza"));
+                perro.setPais(rs.getString("Pais"));
+                perro.setGrupoFCI(rs.getString("Grupo"));
+                perro.setSeccionFCI(rs.getString("Seccion"));
+                perro.setPelo(rs.getString("Pelo"));
+                perro.setColor(rs.getString("Color"));
+                perro.setEspalda(rs.getString("Espalda"));
+                perro.setLomo(rs.getString("Lomo"));
+                perro.setCola(rs.getString("Cola"));
+                perro.setPecho(rs.getString("Pecho"));
+                listaPerros.add(perro);
+
+            }
+            st.close();
+            Conexion.desconectar();
+
+        } catch (SQLException ex) {
+            System.out.println("Error al consultar todos los perros: " + ex.getMessage());
+        }
+
+        return listaPerros;
+    }
+
+    public boolean eliminarPerro(String raza) {
+        String consulta = "DELETE FROM Perros where raza='" + raza + "'";
+        try {
+            con = Conexion.getConexion();
+            st = con.createStatement();
+            st.executeUpdate(consulta);
+            st.close();
+            Conexion.desconectar();
+            return true;
+        } catch (SQLException ex) {
+            System.out.println("No se pudo realizar la eliminacion");
+        }
+        return false;
+    }
+    
+    public void modificarPerro(Perro perro) {
+        
+        String actualizar = "UPDATE Perros SET pais='" + perro.getPais() + "', grupoFCI='" + perro.getGrupoFCI() + "', seccionFCI='" + perro.getSeccionFCI() + "', "
+            + "pelo='" + perro.getPelo() + "', color='" + perro.getColor() + "', espalda='" + perro.getEspalda() + "', lomo='" + perro.getLomo() + "', "
+            + "cola='" + perro.getCola() + "', pecho='" + perro.getPecho() + "' WHERE raza='" + perro.getRaza() + "'";
+        try {
+            con = Conexion.getConexion();
+            st = con.createStatement();
+            st.executeUpdate(actualizar);
+            st.close();
+            Conexion.desconectar();
+            System.out.println("Modificacion hecha");
+        } catch (SQLException ex) {
+            System.out.println("No se pudo realizar la modifcacion");
+        }
+    }
+    
 }
