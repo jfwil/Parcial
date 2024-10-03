e863dbff16534d1686d04b6033643b179d9b685a
Santiago <santiago13012003@gmail.com>
2024-10-02 20:14:19 -0500

    creacion base datos y correcion en properties

--- Parcial1/src/edu/avanzada/parcial/control/Launcher.java
+++ Parcial1/src/edu/avanzada/parcial/control/Launcher.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package edu.avanzada.parcial.control;
+package Control;
 
-/**
- *
- * @author asfak
- */
 public class Launcher {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+            
+        ConexionPr concon = new ConexionPr();
+        
+        CargaPr.cargarPropiedadesIniciales();
+        concon.crearBaseDatos();
     }
-    
 }
